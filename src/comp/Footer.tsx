e5190e0603b7f90eab5a0f869705ed85bef0a9a1
Owen Dearman <owendearman@gmail.com>
2022-05-25 19:53:56 +0100

    footer

--- src/comp/Footer.tsx
+++ src/comp/Footer.tsx
@@ -1,3 +1,27 @@
 export function Footer(): JSX.Element {
-  return <div></div>;
+  return (
+    <footer>
+      <p>
+        The source code for this project can be found{" "}
+        <a
+          target="_blank"
+          href="https://github.com/owen-dearman/project-portfolio"
+        >
+          here
+        </a>
+        .{" "}
+      </p>
+      <p>
+        With thanks to Jacob Cutts for his implementation of p5.js in React (
+        <a target="_blank" href="https://github.com/JacobCutts13/p5-react">
+          here
+        </a>
+        ).{" "}
+      </p>
+      <p>
+        Thank you to Academy for teaching me how to code, and to Neil Bogie for
+        the creative coding sessions!
+      </p>
+    </footer>
+  );
 }
